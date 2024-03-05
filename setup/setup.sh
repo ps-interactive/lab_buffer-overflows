c10ac876038a4fa1ecb93e66a448cfc59e40cb1e
West Shepherd <wshepherd0010@gmail.com>
2024-03-04 20:14:49 -0500

    updated proxy config

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,7 +4,12 @@ function LabSetup() {
   # LabSetup properties
   local _Title="Exploit Development Lab: Buffer Overflows";
   local _Flag="/tmp/.setup-complete";
+  local _Stat="/psorceri";
   local _Verbose="true";
+  local _SSLProxy="$1";
+  local _Proxy="$2";
+  local _Before="INSTALLING";
+  local _Now="";
   local -a _Apps=(
     "gcc"
     "gcc-multilib"
@@ -19,49 +24,60 @@ function LabSetup() {
     "net-tools"
     "python3-pip"
   );
-
-  # Method to display informational messages
-  Info() {
-    [[ -z "$1" ]] || echo "INFO ($(date)): ${@}";
-    return 0;
-  }
-  # Method to display verbose messages for debugging
-  Debug() {
-    if [[ "${_Verbose}" ]]; then
-      [[ -z "$1" ]] || echo "DEBUG ($(date)): ${@}";
-    fi;
-    return 0;
-  }
-  # Method to display error messages
-  Error() {
-    [[ -z "$1" ]] || echo "ERROR ($(date)): ${@}";
-    return 1;
-  }
   # Method to check if a file path exists
   Exists() {
     test -f $1 || return 1;
     return 0;
   }
+  # Method to set status during setup
+  Status(){
+    _Before="${_Now}";
+    _Now="${@}";
+    if [[ ! -z "${_Before}" ]]; then
+      if (Exists "${_Stat}/${_Before}"); then
+        rm "${_Stat}/${_Before}";
+      fi;
+    fi;
+    touch "${_Stat}/${_Now}";
+    echo "${_Now}";
+    return 0;
+  }
+  # Method to display informational messages
+  Info() {
+    [[ -z "$1" ]] || Status "INFO ($(date)): ${@}";
+    return 0;
+  }
+  # Method to display verbose messages for debugging
+  Debug() {
+    if [[ "${_Verbose}" ]]; then
+      [[ -z "$1" ]] || Status "DEBUG ($(date)): ${@}";
+    fi;
+    return 0;
+  }
+  # Method to display error messages
+  Error() {
+    [[ -z "$1" ]] || Status "ERROR ($(date)): ${@}";
+    return 1;
+  }
   # Method to install packages and dependencies
   Install() {
     Info "Setting up APT repository";
-    export DEBIAN_FRONTEND="noninteractive";
-    export NEEDRESTART_MODE="l";
-
     Info "Updating repository and installing dependencies";
-    apt update && apt update && apt install ${_Apps[@]} -y --force-yes;
+    http_proxy=${_SSLProxy} apt update;
+    http_proxy=${_SSLProxy} apt update;
+    http_proxy=${_SSLProxy} DEBIAN_FRONTEND=noninteractive apt -y --force-yes install ${_Apps[@]};
 
     Info "Setting up GDB peda extension";
-    git clone https://github.com/longld/peda.git ~/peda
+    git -c http.proxy=${_Proxy} clone https://github.com/longld/peda.git ~/peda
     echo "source ~/peda/peda.py" >> ~/.gdbinit
 
     Info "Setting up boofuzz and ROPgadget packages"
-    python3 -m pip install boofuzz ROPgadget;
+    python3 -m pip --proxy ${_SSLProxy} install boofuzz ROPgadget;
     return 0;
   }
   # Method to download a file from the web
   DownloadFile(){
-    curl -A "PSLabs" -o $2 -skL $1;
+    curl --proxy ${_SSLProxy} -o "$2" -skL "$1";
     return 0;
   }
   # Method to download the lab files from the Github repository
@@ -71,20 +87,21 @@ function LabSetup() {
     local _server="${_url}/setup/lin-server";
     local _url="${_url}/exploitation";
     local -a _files=(
-    "fuzz-server.py"
-    "linux-exploit.py"
-    "windows-exploit.py"
-    "encode-command.py"
-    "reverse-tcp.ps1"
-    "x86-exec-cradle.asm"
-    "x86-reverse-tcp.asm"
+      "exploit-generator.sh"
+      "fuzz-server.py"
+      "linux-exploit.py"
+      "windows-exploit.py"
+      "encode-command.py"
+      "reverse-tcp.ps1"
+      "x86-exec-cradle.asm"
+      "x86-reverse-tcp.asm"
     );
 
     test -d /opt/lab || mkdir -p /opt/lab;
     DownloadFile "${_server}" "/opt/lab/lin-server";
     for _file in ${_files[@]}; do
-    DownloadFile "${_url}/${_file}" "/opt/lab/${_file}";
-    chmod +x "/opt/lab/${_file}";
+      DownloadFile "${_url}/${_file}" "/opt/lab/${_file}";
+      chmod +x "/opt/lab/${_file}";
     done;
     return 0;
   }
@@ -120,10 +137,13 @@ EOF
     Info "Setting up learner environment";
     chmod +x /opt/lab/*
     for _file in /opt/lab/*.py; do
-    ln -s "${_file}" /usr/bin/$(basename "${_file}");
+      ln -s "${_file}" /usr/bin/$(basename "${_file}");
     done;
     for _file in /opt/lab/*.ps1; do
-    ln -s "${_file}" /tmp/$(basename "${_file}");
+      ln -s "${_file}" /tmp/$(basename "${_file}");
+    done;
+    for _file in /opt/lab/*.sh; do
+      ln -s "${_file}" /tmp/$(basename "${_file}");
     done;
     return 0;
   }
@@ -146,4 +166,4 @@ EOF
   Setup;
 }
 
-LabSetup;
+LabSetup "$1" "$2" &>/tmp/setup.log ; cat /tmp/setup.log;
